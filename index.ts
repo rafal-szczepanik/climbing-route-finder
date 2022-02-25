9535eb4300d86bd45066d17a27033d95500e9118
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-02-25 14:29:05 +0100

    update index.ts

--- index.ts
+++ index.ts
@@ -1,23 +1,38 @@
 import * as express from 'express';
-import {json, static as eStatic} from 'express'
+import {static as eStatic, urlencoded} from 'express'
 import 'express-async-errors';
 import './utils/db'
+import * as methodOverride from "method-override"
 import {join} from 'path';
 import {engine} from 'express-handlebars'
 import {notFound} from "./middlewares/notFound";
+import {homeRouter} from "./routers/home";
+import {landRouter} from './routers/land';
+import {areaRouter} from "./routers/area";
+import {climbingRouteRouter} from "./routers/climbing_route";
+import {rockArenaRouter} from "./routers/rock_area";
+import {rockRouter} from "./routers/rock";
 
 const app = express();
 
 //Express middlewares
 app.use(eStatic(join(__dirname, 'public')))
-app.use(json())
-
+app.use(urlencoded({
+    extended: true
+}))
 //View engine
 app.engine('.hbs', engine({
     extname: '.hbs',
     // helpers: handlebarsHelpers
 }))
 app.set('view engine', '.hbs')
+
+app.use('/', homeRouter)
+app.use('/land', landRouter)
+app.use('/land/area', areaRouter)
+app.use('/land/area/rock_area', rockArenaRouter)
+app.use('/land/area/rock_area/rock', rockRouter)
+app.use('/land/area/rock_area/rock/climbing_route', climbingRouteRouter)
 
 //Error middlewares
 app.use(notFound)
